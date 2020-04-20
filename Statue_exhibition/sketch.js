8d03a2f67918d4e7704671766cf127b57dfdd27e
Ester Marie Aagaard <36190068+estermarieAA@users.noreply.github.com>
2020-04-20 18:04:54 +0900

    changes in sketch file

--- Statue_exhibition/sketch.js
+++ Statue_exhibition/sketch.js
@@ -1,24 +1,22 @@
 let complete;
-let mov;
-let img;
 let rot = 0;
+let rotatebox= 0;
 
 function preload(){
   complete = loadModel("assets/tinker.obj", true);
 
-   img = loadImage("assets/img.png", true);
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
 
 
-
-  // enable easy cam
-createEasyCam();
-
-// suppress right-click context menu
-document.oncontextmenu = function() { return false; }
+//
+//   // enable easy cam
+// createEasyCam();
+//
+// // suppress right-click context menu
+// document.oncontextmenu = function() { return false; }
 
 }
 
@@ -27,68 +25,58 @@ background(0);
 
 
 
-if(rot === 1){
-  directionalLight(255, 0,0, -1, 0, 0);
-} else if( rot ===2){
-  directionalLight(255, 255,0, 1, 0, 0);
-} else if(rot===3){
-  directionalLight(255, 0,255, 0, -1, 0);
-} else if( rot ===4){
-  directionalLight(0, 255,0, 0, 1, 0);
-} else if( rot ===5){
-  directionalLight(0, 255,255, 0, 0, -1);
-} else if ( rot === 6){
-  directionalLight(255, 0,255, 0, 0, 1);
+lighting();
+
+//placement and rotation of box
+translate(0,50,-250);
+rotatebox += 0.2;
+rotateY(radians(rotatebox));
+
+boxwithsign();
+
 }
 
-if(frameCount%15==0 === true){
-  rot ++
+function lighting (){
+
+  shininess(20);
+
+  if(rot === 1){
+    directionalLight(255, 0,0, -1, 0, 0);
+  } else if( rot ===2){
+    directionalLight(255, 255,0, 1, 0, 0);
+  } else if(rot===3){
+    directionalLight(255, 0,255, 0, -1, 0);
+  } else if( rot ===4){
+    directionalLight(0, 255,0, 0, 1, 0);
+  } else if( rot ===5){
+    directionalLight(0, 255,255, 0, 0, -1);
+  } else if ( rot === 6){
+    directionalLight(255, 0,255, 0, 0, 1);
+  }
+
+  if(frameCount%15==0 === true){
+    rot ++
+  }
+
+  if(rot>6 === true){
+    rot=0;
+  }
 }
 
-
-if(rot>6 === true){
-  rot=0;
-}
-//lights();
-//ambientLight(255,255,0);
-
-// directionalLight(255, 0,0, -1, 0, 0);
-//   directionalLight(255, 0,0, 1, 0, 0);
-//  directionalLight(255, 0,0, 0, -1, 0);
-//   directionalLight(255, 0,0, 0, 1, 0);
-//  directionalLight(255, 0,0, 0, 0, -1);
-//  directionalLight(255, 0,0, 0, 0, 1);
-
-  // let locX = mouseX - width / 2;
-  // let locY = mouseY - height / 2;
-  //
-  //
-  //
-  // pointLight(255, 255, 0, locX, locY, 50);
-
-
-
-
-translate(0,50,0);
-
+function boxwithsign(){
   //floor
   push();
-  //ambientMaterial(255,255,0,30);
   ambientMaterial(255,60);
   translate(0,200,0);
   rotateX(radians(90));
-  // tint(255, 10);
-  // texture(img);
   box(700,500,3,0,0);
   pop();
 
   //ceiling
   push();
   ambientMaterial(255,60);
-  // ambientMaterial(255,255,0,30);
   translate(0,-300,0);
   rotateX(radians(90));
-    // texture(img);
   box(700,500,3);
   pop();
 
@@ -96,18 +84,13 @@ translate(0,50,0);
   push();
   translate(0,-47,-250)
   ambientMaterial(255,60);
-  // ambientMaterial(255,255,0,30);
-    // texture(img);
   box(700,500,3);
   pop();
 
   //frontwall
   push();
   translate(0,-47,250)
- ambientMaterial(255,60);
-  // ambientMaterial(255,255,0,30);
-
-    // texture(img);
+  ambientMaterial(255,60);
   box(700,500,3);
   pop();
 
@@ -115,11 +98,7 @@ translate(0,50,0);
   push();
   translate(-350,-47,0)
   rotateY(radians(90));
-  //normalMaterial();
-  // fill(0);
   ambientMaterial(255,60);
-  // ambientMaterial(255,255,0,30);
-    // texture(img);
   box(500,500,3);
   pop();
 
@@ -127,11 +106,7 @@ translate(0,50,0);
   push();
   translate(350,-47,0)
   rotateY(radians(90));
-  //normalMaterial();
-  // fill(0);
   ambientMaterial(255,60);
-  // ambientMaterial(255,255,0,30);
-    // texture(img);
   box(500,500,3);
   pop();
 
@@ -142,9 +117,8 @@ translate(0,50,0);
   rotateX(radians(180));
   scale(3);
   translate(0,10,-10);
-  //texture(img);
+  //specular material makes it easier to see were the lights come from
+  specularMaterial(255);
   model(complete);
   pop();
-
-
 }
